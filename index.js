8df184b37676289f3741afcc4eefa110da6594e7
RavinakS <ravinasingh697@gmail.com>
2021-09-11 22:22:31 +0530

    **

--- index.js
+++ index.js
@@ -50,51 +50,98 @@ function valid_input(user_input){
     return false;
   }else if(user_input.length === 1){
     return true;
-  }else{
+  }else if(user_input === 'hint'){
+    return 'hint'
+  }
+  else{
     return false;
   }
 }
 
-function hangman(secret_word, images){
-  console.log("Welcome to the game, Hangman!");
-  console.log(`I am thinking of a word that is ${secret_word.length} letters long.`);
-  console.log("");
-  letters_guessed = []
+function hint_letter(secret_word, guessed_word){
+  for(letter of secret_word){
+    if(!guessed_word.includes(letter)){
+      return letter;
+    }
+  }
+}
 
-  remaining_lives = 8;
+function game(secret_word, remaining_lives, images){
+  letters_guessed = []
+  // remaining_lives = 8;
   while(remaining_lives>=1){
     available_letters = get_available_letters(letters_guessed)
+    console.log("");
     console.log(`Available letters: ${available_letters}`);
 
     word_guessed = is_word_guessed(secret_word, letters_guessed);
     if(word_guessed){
+      console.log("");
       console.log(" * * Congratulations, you won! * * ");
       console.log("");
       break;
     }
+    console.log("");
     guess = readline.question("Please guess a letter: ")
-    letter = guess.toLowerCase()
-    if(valid_input(letter) && secret_word.includes(letter)){
+    letter = guess.toLowerCase();
+    valid = valid_input(letter);
+    if(valid === 'hint'){
+      h_letter = hint_letter(secret_word, letters_guessed);
+      letters_guessed.push(letter);
+      console.log("");
+      console.log(`Here you go: ${get_guessed_word(secret_word, letters_guessed)}`);
+      console.log("");
+    }else if(valid && secret_word.includes(letter)){
       if(!letters_guessed.includes(letter)){
         letters_guessed.push(letter);
+        console.log("");
         console.log(`Good guess: ${get_guessed_word(secret_word, letters_guessed)}`);
         console.log("");
       }else{
+        console.log("");
         console.log("Guessed one.");
         console.log(images[images.length - remaining_lives]);
         remaining_lives--;
       }
     }else{
       if(valid_input(letter) === false){
+        console.log("");
         console.log("Invalid Input");
         console.log(images[images.length - remaining_lives]);
         remaining_lives--;
       }else{
+        console.log("");
         console.log(`Oops! That letter is not in my word:  ${get_guessed_word(secret_word, letters_guessed)}`)
         console.log("");
         console.log(images[images.length - remaining_lives]);
         remaining_lives--;
       }
+    }
+  }
+}
+
+function hangman(secret_word, images){
+  console.log("");
+  console.log("* * Welcome to the game, Hangman! * *");
+  console.log("");
+  console.log(`The word is ${secret_word.length} letters long.`);
+  console.log("");
+
+  while(true){
+    user = readline.question('Difficulty Level (Easy/Medium/Hard)> ');
+    if(user.toLowerCase() === 'easy'){
+      game(secret_word, 8, images);
+      break;
+    }else if(user.toLowerCase() === 'medium'){
+      game(secret_word, 6, images);
+      break;
+    }else if(user.toLowerCase() === 'hard'){
+      game(secret_word, 4, images);
+      break;
+    }else{
+      console.log("");
+      console.log("Please tell in which level you want to play.");
+      console.log("");
     }
   }
 }
