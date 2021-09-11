015b9664a90f0e5bc2bb9a9b3b17cb58075adf83
RavinakS <ravinasingh697@gmail.com>
2021-09-11 10:55:05 +0530

    {}

--- images.js
+++ images.js
@@ -35,7 +35,7 @@ IMAGES = [`
     +---+
     |   |
     0   |
-   /|\  |
+   /|\\  |
         |
         |
         =========`, `
@@ -43,7 +43,7 @@ IMAGES = [`
     +---+
     |   |
     0   |
-   /|\  |
+   /|\\  |
     |   |
         |
         =========`, `
@@ -51,7 +51,7 @@ IMAGES = [`
     +---+
     |   |
     0   |
-   /|\  |
+   /|\\  |
     |   |
    /    |
         =========`, `
@@ -59,9 +59,12 @@ IMAGES = [`
     +---+
     |   |
     0   |
-   /|\  |
+   /|\\  |
     |   |
-   / \  |
+   / \\  |
         =========`]
+
+// '\' is a escape character, so if I want to print the '\' then I have to escape that too
+// that's why '\\' fist backward slash is to print and second one to escape the first backward slash
 
 module.exports = IMAGES ;
