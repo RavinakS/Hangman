41bec0abfe094a20f8df17fae49f65afb72ab157
RavinakS <ravinasingh697@gmail.com>
2021-09-11 22:39:54 +0530

    **

--- words.js
+++ words.js
@@ -1,23 +1,21 @@
-// import string
-// import random
+const fs = require('fs');
 
 function load_words(){
-    // Ye function kaafi jayada words ko load karne mai help karega
-
-    const word_list = ["navgurukul", "learning", "kindness"];
+    data = fs.readFileSync('words.txt', 'utf8');
+    const word_list = data.split(' ');
+    // console.log(word_list);
+    // const word_list = ["navgurukul", "learning", "kindness"];
     return word_list
 }
 
 function choose_word(){
-    // word_list (list): list of words (strings)
-    // ye function ek word randomly return karega
-
     word_list = load_words()
-    // secret_word = random.choice(word_list)
     var secret_word = word_list[Math.floor((Math.random()*word_list.length))];
     secret_word = secret_word.toLowerCase()
 
     return secret_word
 }
-    
+
+// choose_word();
+
 module.exports = choose_word;
